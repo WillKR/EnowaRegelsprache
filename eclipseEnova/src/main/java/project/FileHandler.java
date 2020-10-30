c4e0eeaf3e13d6cafa9d40635c0cba5a2bfa0f36
William Kistenberger <william.kistenberger@empolis.com>
2020-10-30 16:54:07 +0100

    stopped multiple dots in file errors

--- eclipseEnova/src/main/java/project/FileHandler.java
+++ eclipseEnova/src/main/java/project/FileHandler.java
@@ -30,12 +30,16 @@ public class FileHandler {
 	
 	public static Word lineDevision(String line) {
 		String[] wordParts = line.split("=");
+		if (wordParts.length!=2) throw new IllegalArgumentException("Fehlerhafte Argumente");
+
 		String type;
 		if (wordParts[1].charAt(0)=='\"') {
 			type = "text";
 		}else {
 			type = "number";
 		}
+	    System.out.println(wordParts[0]);
+		if (!wordParts[0].matches("[\\w]([\\d\\w]|(\\.?)[\\d\\w])*")) throw new IllegalArgumentException("Fehlerhafte Argumente");
 		Word word = new Word(type, wordParts[0]);
 		word.allocate(wordParts[1]);
 		return word;
